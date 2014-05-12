5a46cd11e8eb84b87baea593068f12202204e0e4
Hans Gillis <hans.gillis.work@gmail.com>
2014-05-12 12:22:20 +0200

    Cross-platform 0.3
    Fading doesn't work on Android

---
+++ engine/utilities/audio.py
@@ -0,0 +1,37 @@
+import pygame
+from pygame.locals import *
+
+try:
+    import android
+except ImportError:
+    android = None
+
+try:
+    import pygame.mixer as mixer
+except ImportError:
+    import android.mixer as mixer
+
+class Audio(object):
+    def __init__(self):
+        try:
+            mixer.init(44100, -16, 2, 1024)
+        except:
+            pass
+
+    def sound(self, pathfile):
+        return mixer.Sound(pathfile)
+
+    def play(self, sound_object):
+        if not mixer.get_busy():
+            sound_object.play()
+
+    def music(self, pathfile):
+        mixer.music.load(pathfile)
+
+    def loop(self):
+        if android:
+            if mixer.music.channel.get_busy() == False:
+                mixer.music.play()
+        else:
+           if mixer.music.get_busy() == False:
+              mixer.music.play()
